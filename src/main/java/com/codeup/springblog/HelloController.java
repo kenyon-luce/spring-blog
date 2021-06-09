d8c820672194596a2c7bb964a18b44981f430e7d
Kenyon Luce <kenyon.luce02@gmail.com>
2021-06-09 11:20:44 -0500

    added hello controller with name

--- src/main/java/com/codeup/springblog/HelloController.java
+++ src/main/java/com/codeup/springblog/HelloController.java
@@ -1,13 +1,20 @@
 package com.codeup.springblog;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
     @GetMapping("/hello")
     @ResponseBody
-    public String hello(){
+    public String hello() {
         return "Hello from Spring Boot!";
+    }
+
+    @GetMapping("/hello/{name}")
+    @ResponseBody
+    public String helloName(@PathVariable String name){
+        return "Hello from Spring Boot to " + name + "!";
     }
 }
