9ad78f99554e9f5ff510d2f2c4abb8f7394c7b56
Kenyon Luce <kenyon.luce02@gmail.com>
2021-06-09 11:29:56 -0500

    added post methods for spring exercise

--- src/main/java/com/codeup/springblog/PostController.java
+++ src/main/java/com/codeup/springblog/PostController.java
@@ -1,2 +1,34 @@
-package com.codeup.springblog;public class PostController {
+package com.codeup.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PostController {
+    @GetMapping("/posts")
+    @ResponseBody
+    public String posts(){
+        return "posts index page";
+    }
+
+    @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String postId(@PathVariable int id){
+        return "view an individual post with id: " + id;
+    }
+
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String postGetCreate(){
+        return "view the form for creating the post";
+    }
+
+    @PostMapping("/posts/create")
+    @ResponseBody
+    public String postPostCreate(){
+        return "create a new post";
+    }
 }
