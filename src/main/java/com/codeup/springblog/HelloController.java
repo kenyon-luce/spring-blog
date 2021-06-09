5d0f02835e7f85df39c155d6b9d4262e453af7bc
Kenyon Luce <kenyon.luce02@gmail.com>
2021-06-09 10:44:13 -0500

    added hello controller

---
+++ src/main/java/com/codeup/springblog/HelloController.java
@@ -0,0 +1,13 @@
+package com.codeup.springblog;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "Hello from Spring Boot!";
+    }
+}
