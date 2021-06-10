4f36eaa90efcd408ed064da849bf676028e21dab
Kenyon Luce <kenyon.luce02@gmail.com>
2021-06-10 16:09:44 -0500

    attempted first exercise

--- src/main/java/com/codeup/springblog/RollDice.java
+++ src/main/java/com/codeup/springblog/RollDice.java
@@ -2,18 +2,21 @@ package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RollDice {
-    @GetMapping("/roll-dice/n")
-    public String showForm(){
+    @GetMapping("/roll-dice")
+    public String showJoinForm() {
         return "guess";
     }
 
-    @PostMapping("/roll-dice/n")
-    public String guess(@RequestParam(name="n") String n, Model model){
-        model.addAttribute("n", n);
+    @PostMapping("/roll-dice/{n}")
+    public String guess(@PathVariable int n, Model model) {
+        model.addAttribute("n", "You guessed " + n + "!");
         return "guess";
     }
 }
