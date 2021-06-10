882a3170adf3c7be534f0d4e8cfd4f8e27bf5fed
Kenyon Luce <kenyon.luce02@gmail.com>
2021-06-10 15:17:16 -0500

    updated methods

--- src/main/java/com/codeup/springblog/RollDice.java
+++ src/main/java/com/codeup/springblog/RollDice.java
@@ -2,14 +2,18 @@ package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class RollDice {
-    @GetMapping("/roll-dice/{n}")
-    public String guess(@PathVariable int n, Model model){
-        model.addAttribute("Guess", n);
+    @GetMapping("/roll-dice/n")
+    public String showForm(){
+        return "guess";
+    }
+
+    @PostMapping("/roll-dice/n")
+    public String guess(@RequestParam(name="n") String n, Model model){
+        model.addAttribute("n", n);
         return "guess";
     }
 }
