7b70e131127c309f80636023c6f4b06e0c94bb59
Kenyon Luce <kenyon.luce02@gmail.com>
2021-06-10 14:34:17 -0500

    notes wip

--- src/main/java/com/codeup/springblog/UserController.java
+++ src/main/java/com/codeup/springblog/UserController.java
@@ -1,13 +1,20 @@
-package com.codeup.springblog;//package com.codeup.springblog;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class UserController {
-
-    @GetMapping("/users/all")
-    public String getUsers() {
-        return "users";
-    }
-}
+//package com.codeup.springblog;//package com.codeup.springblog;
+//
+//import org.apache.catalina.User;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@Controller
+//public class UserController {
+//
+//    @GetMapping("/users/all")
+//    public String getUsers() {
+//        List<User> users = new ArrayList<>();
+//        users.add(new User("Kenyon", "Luce"));
+//
+//        return "users";
+//    }
+//}
