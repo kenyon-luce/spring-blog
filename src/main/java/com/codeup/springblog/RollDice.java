c853bef9c35b6e63cc28e2066f9bb1ceec818ff7
Kenyon Luce <kenyon.luce02@gmail.com>
2021-06-10 15:01:16 -0500

    getting and printing number typed in url

--- src/main/java/com/codeup/springblog/RollDice.java
+++ src/main/java/com/codeup/springblog/RollDice.java
@@ -1,4 +1,15 @@
 package com.codeup.springblog;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
 public class RollDice {
+    @GetMapping("/roll-dice/{n}")
+    public String guess(@PathVariable int n, Model model){
+        model.addAttribute("Guess", n);
+        return "guess";
+    }
 }
