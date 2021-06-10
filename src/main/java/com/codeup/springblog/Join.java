0ba3231840ccc6ea5a1300d37b0f054b0051c047
Kenyon Luce <kenyon.luce02@gmail.com>
2021-06-10 15:20:37 -0500

    added join notes

---
+++ src/main/java/com/codeup/springblog/Join.java
@@ -0,0 +1,21 @@
+package com.codeup.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class Join {
+    @GetMapping("/join")
+    public String showJoinForm() {
+        return "join";
+    }
+
+    @PostMapping("/join")
+    public String joinCohort(@RequestParam(name = "cohort") String cohort, Model model) {
+        model.addAttribute("cohort", "Welcome to " + cohort + "!");
+        return "join";
+    }
+}
