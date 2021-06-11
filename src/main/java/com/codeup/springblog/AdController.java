c75416fa63ca7dddf536bb11b1f073c27f20253e
Kenyon Luce <kenyon.luce02@gmail.com>
2021-06-11 15:09:40 -0500

    updated code

--- src/main/java/com/codeup/springblog/AdController.java
+++ src/main/java/com/codeup/springblog/AdController.java
@@ -15,7 +15,7 @@ public class AdController {
     @GetMapping("/ads")
     public String index(Model model) {
         model.addAttribute("ads", adDao.findAll());
-        model.addAttribute("topAd", adDao.findByTitle("bicycle north side"));
+        model.addAttribute("topAd", adDao.findByTitle("bicycle north side")); //searches specific ad
         model.addAttribute("searchAd", adDao.findByTitleLike("%fridge%")); //has to include the % to work
         return "adsIndex";
     }
