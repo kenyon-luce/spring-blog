e794e82be681aa0c0a2225ac8ff5d88b47e4d02f
Kenyon Luce <kenyon.luce02@gmail.com>
2021-06-11 10:44:47 -0500

    updated code

--- src/main/java/com/codeup/springblog/Post.java
+++ src/main/java/com/codeup/springblog/Post.java
@@ -4,6 +4,11 @@ public class Post {
     private String title;
     private String body;
 
+    public Post(String title, String body){
+        this.title = title;
+        this.body = body;
+    }
+
     public String getTitle(){
         return this.title;
     }
--- src/main/java/com/codeup/springblog/PostController.java
+++ src/main/java/com/codeup/springblog/PostController.java
@@ -2,16 +2,17 @@ package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class PostController {
-    @GetMapping("/posts")
-    @ResponseBody
+//    @GetMapping("/posts")
+//    @ResponseBody
+    @RequestMapping(path="/posts", method=RequestMethod.GET)
     public String posts(){
+        Post post = new Post("title", "body");
+        post.setTitle("title");
+        post.setBody("body");
         return "posts index page";
     }
 
