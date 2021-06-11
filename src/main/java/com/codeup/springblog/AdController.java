95e8126e64052b4e4d54ca05ef62236e9af3d61e
Kenyon Luce <kenyon.luce02@gmail.com>
2021-06-11 14:12:58 -0500

    listing ads through spring

---
+++ src/main/java/com/codeup/springblog/AdController.java
@@ -0,0 +1,20 @@
+package com.codeup.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdController {
+    private final AdRepository adDao;
+
+    public AdController(AdRepository adDao){
+        this.adDao = adDao;
+    }
+
+    @GetMapping("/ads")
+    public String index(Model model) {
+        model.addAttribute("ads", adDao.findAll());
+        return "adsIndex";
+    }
+}
