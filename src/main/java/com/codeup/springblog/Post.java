e655714310e15dd61bdabcd119f90dbdbd2870d5
Kenyon Luce <kenyon.luce02@gmail.com>
2021-06-11 10:10:08 -0500

    working on posts

--- src/main/java/com/codeup/springblog/Post.java
+++ src/main/java/com/codeup/springblog/Post.java
@@ -1,4 +1,20 @@
 package com.codeup.springblog;
 
 public class Post {
+    private String title;
+    private String body;
+
+    public String getTitle(){
+        return this.title;
+    }
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public String getBody(){
+        return this.body;
+    }
+    public void setBody(String body){
+        this.body = body;
+    }
 }
--- src/main/java/com/codeup/springblog/PostController.java
+++ src/main/java/com/codeup/springblog/PostController.java
@@ -1,6 +1,7 @@
 package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
